9e0554613d9daa189380cbae40329b0e5f40e96a
ViniciusGiroto <viniciusgiroto@outlook.com>
2024-08-04 09:21:14 -0300

    Grouping equal values next to each other Equal values following each other will now be grouped to produce smaller mif outputs Support for float types

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
-#![feature(integer_sign_cast)]
+#![feature(integer_sign_cast, f16, f128)]
+#![cfg_attr(feature = "f16", feature(f16))]
+#![cfg_attr(feature = "f128", feature(f128))]
 
 pub mod parser;
 pub mod radix;
--- src/writer.rs
+++ src/writer.rs
@@ -22,6 +22,121 @@ impl<'a, T: ToMif> Mif<'a, T> {
     }
 }
 
+macro_rules! write_data {
+    ($f:expr, $data:expr, $index:expr, $value:expr, signed) => {{
+        let Some(first) = $data.next() else {
+            return Ok(());
+        };
+
+        let group = $data.try_fold(
+            GroupState {
+                start: 0,
+                length: 1,
+                value: first.1.signed(),
+            },
+            |mut acc, (index, value)| {
+                let value = value.signed();
+
+                if acc.value == value {
+                    acc.length += 1;
+                } else {
+                    if index - acc.start > 1 {
+                        write!($f, "[");
+                        write!($f, $index, acc.start);
+                        write!($f, "..");
+                        write!($f, $index, index - 1);
+                        write!($f, "]:");
+                    } else {
+                        write!($f, $index, acc.start);
+                        write!($f, ":");
+                    }
+
+                    write!($f, $value, acc.value);
+                    write!($f, ";\n");
+
+                    acc.start = index;
+                    acc.value = value;
+                    acc.length = 1;
+                }
+                Ok(acc)
+            },
+        )?;
+
+        if group.length > 1 {
+            write!($f, "[");
+            write!($f, $index, group.start);
+            write!($f, "..");
+            write!($f, $index, group.start + group.length - 1);
+            write!($f, "]:");
+        } else {
+            write!($f, $index, group.start);
+            write!($f, ":");
+        }
+
+        write!($f, $value, group.value);
+        write!($f, ";\n");
+    }};
+    ($f:expr, $data:expr, $index:expr, $value:expr) => {{
+        let Some(first) = $data.next() else {
+            return Ok(());
+        };
+
+        let group = $data.try_fold(
+            GroupState {
+                start: 0,
+                length: 1,
+                value: first.1.bits(),
+            },
+            |mut acc, (index, value)| {
+                let value = value.bits();
+
+                if acc.value == value {
+                    acc.length += 1;
+                } else {
+                    if index - acc.start > 1 {
+                        write!($f, "[");
+                        write!($f, $index, acc.start);
+                        write!($f, "..");
+                        write!($f, $index, index - 1);
+                        write!($f, "]:");
+                    } else {
+                        write!($f, $index, acc.start);
+                        write!($f, ":");
+                    }
+
+                    write!($f, $value, acc.value);
+                    write!($f, ";\n");
+
+                    acc.start = index;
+                    acc.value = value;
+                    acc.length = 1;
+                }
+                Ok(acc)
+            },
+        )?;
+
+        if group.length > 1 {
+            write!($f, "[");
+            write!($f, $index, group.start);
+            write!($f, "..");
+            write!($f, $index, group.start + group.length - 1);
+            write!($f, "]:");
+        } else {
+            write!($f, $index, group.start);
+            write!($f, ":");
+        }
+
+        write!($f, $value, group.value);
+        write!($f, ";\n");
+    }};
+}
+
+struct GroupState<T: PartialEq> {
+    start: usize,
+    length: usize,
+    value: T,
+}
+
 impl<'a, T> Display for Mif<'a, T>
 where
     T: ToMif,
@@ -36,140 +151,172 @@ where
         let data_radix = self.data_radix;
         let mut data = self.data.iter().enumerate();
 
-        f.write_fmt(format_args!("DEPTH = {depth};\nWIDTH = {width};\nADDRESS_RADIX = {address_radix};\nDATA_RADIX = {data_radix};\nCONTENT BEGIN\n"))?;
+        f.write_fmt(format_args!("DEPTH={depth};\nWIDTH={width};\nADDRESS_RADIX={address_radix};\nDATA_RADIX={data_radix};\nCONTENT BEGIN\n"))?;
 
         match (address_radix, data_radix) {
-            (Radix::Bin, Radix::Bin) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!(
-                    "{index:0bin_width$b} : {value:0bin_width$b};\n",
-                ))
-            })?,
-            (Radix::Bin, Radix::Dec) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!("{index:0bin_width$b} : {value};\n"))
-            })?,
-            (Radix::Bin, Radix::Hex) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!(
-                    "{index:0bin_width$b} : {value:0hex_width$X};\n",
-                ))
-            })?,
-            (Radix::Bin, Radix::Oct) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!(
-                    "{index:0bin_width$b} : {value:0oct_width$o};\n",
-                ))
-            })?,
-            (Radix::Bin, Radix::Uns) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!(
-                    "{index:0bin_width$b} : {};\n",
-                    value.unsigned()
-                ))
-            })?,
+            (Radix::Bin, Radix::Bin) => {
+                write_data!(f, data, "{:0bin_width$b}", "{:0bin_width$b}")
+            }
+            (Radix::Bin, Radix::Dec) => {
+                write_data!(f, data, "{:0bin_width$b}", "{}", signed)
+            }
+            (Radix::Bin, Radix::Hex) => {
+                write_data!(f, data, "{:0bin_width$b}", "{:0hex_width$X}")
+            }
+            (Radix::Bin, Radix::Oct) => {
+                write_data!(f, data, "{:0bin_width$b}", "{:0oct_width$o}")
+            }
+            (Radix::Bin, Radix::Uns) => write_data!(f, data, "{:0bin_width$b}", "{}"),
 
-            (Radix::Hex, Radix::Bin) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!(
-                    "{index:0hex_width$X} : {value:0bin_width$b};\n",
-                ))
-            })?,
-            (Radix::Hex, Radix::Dec) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!("{index:0hex_width$X} : {value};\n",))
-            })?,
-            (Radix::Hex, Radix::Hex) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!(
-                    "{index:0hex_width$X} : {value:0hex_width$X};\n",
-                ))
-            })?,
-            (Radix::Hex, Radix::Oct) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!(
-                    "{index:0hex_width$X} : {value:0oct_width$o};\n",
-                ))
-            })?,
-            (Radix::Hex, Radix::Uns) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!(
-                    "{index:0hex_width$X} : {};\n",
-                    value.unsigned()
-                ))
-            })?,
+            (Radix::Hex, Radix::Bin) => {
+                write_data!(f, data, "{:0hex_width$X}", "{:0bin_width$b}")
+            }
+            (Radix::Hex, Radix::Dec) => {
+                write_data!(f, data, "{:0hex_width$X}", "{}", signed)
+            }
+            (Radix::Hex, Radix::Hex) => {
+                write_data!(f, data, "{:0hex_width$X}", "{:0hex_width$X}")
+            }
+            (Radix::Hex, Radix::Oct) => {
+                write_data!(f, data, "{:0hex_width$X}", "{:0oct_width$o}")
+            }
+            (Radix::Hex, Radix::Uns) => write_data!(f, data, "{:0hex_width$X}", "{}"),
 
-            (Radix::Oct, Radix::Bin) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!(
-                    "{index:0oct_width$o} : {value:0bin_width$b};\n",
-                ))
-            })?,
-            (Radix::Oct, Radix::Dec) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!("{index:0oct_width$o} : {value};\n"))
-            })?,
-            (Radix::Oct, Radix::Hex) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!(
-                    "{index:0oct_width$o} : {value:0hex_width$X};\n",
-                ))
-            })?,
-            (Radix::Oct, Radix::Oct) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!(
-                    "{index:0oct_width$o} : {value:0oct_width$o};\n",
-                ))
-            })?,
-            (Radix::Oct, Radix::Uns) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!(
-                    "{index:0oct_width$o} : {};\n",
-                    value.unsigned()
-                ))
-            })?,
+            (Radix::Oct, Radix::Bin) => {
+                write_data!(f, data, "{:0oct_width$o}", "{:0bin_width$b}")
+            }
+            (Radix::Oct, Radix::Dec) => {
+                write_data!(f, data, "{:0oct_width$o}", "{}", signed)
+            }
+            (Radix::Oct, Radix::Hex) => {
+                write_data!(f, data, "{:0oct_width$o}", "{:0hex_width$X}")
+            }
+            (Radix::Oct, Radix::Oct) => {
+                write_data!(f, data, "{:0oct_width$o}", "{:0oct_width$o}")
+            }
+            (Radix::Oct, Radix::Uns) => write_data!(f, data, "{:0oct_width$o}", "{}"),
 
-            (Radix::Dec | Radix::Uns, Radix::Bin) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!("{index} : {value:0bin_width$b};\n"))
-            })?,
-            (Radix::Dec | Radix::Uns, Radix::Dec) => data
-                .try_for_each(|(index, value)| f.write_fmt(format_args!("{index} : {value};\n")))?,
-            (Radix::Dec | Radix::Uns, Radix::Hex) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!("{index} : {value:0hex_width$X};\n"))
-            })?,
-            (Radix::Dec | Radix::Uns, Radix::Oct) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!("{index} : {value:0oct_width$o};\n"))
-            })?,
-            (Radix::Dec | Radix::Uns, Radix::Uns) => data.try_for_each(|(index, value)| {
-                f.write_fmt(format_args!("{index} : {};\n", value.unsigned()))
-            })?,
+            (Radix::Dec | Radix::Uns, Radix::Bin) => {
+                write_data!(f, data, "{}", "{:0bin_width$b}")
+            }
+            (Radix::Dec | Radix::Uns, Radix::Dec) => {
+                write_data!(f, data, "{}", "{}", signed)
+            }
+            (Radix::Dec | Radix::Uns, Radix::Hex) => {
+                write_data!(f, data, "{}", "{:0hex_width$X}")
+            }
+            (Radix::Dec | Radix::Uns, Radix::Oct) => {
+                write_data!(f, data, "{}", "{:0oct_width$o}")
+            }
+            (Radix::Dec | Radix::Uns, Radix::Uns) => write_data!(f, data, "{}", "{}"),
         }
 
         f.write_str("END;")
     }
 }
 
-pub trait ToMif: Octal + Binary + UpperHex + Display {
+pub trait ToMif {
     const WIDTH: usize;
 
-    type Unsigned: Display;
+    type SignedBinary: Display + PartialEq;
+    type Binary: Octal + Binary + UpperHex + Display + PartialEq;
 
-    fn unsigned(&self) -> Self::Unsigned;
+    fn signed(&self) -> Self::SignedBinary;
+    fn bits(&self) -> Self::Binary;
 }
 
 macro_rules! impl_to_mif {
-    ($t:ty, $u:ty) => {
-        impl ToMif for $t {
-            type Unsigned = $u;
+    ($s:ty, $u:ty, $f:ty) => {
+        impl_to_mif!($s, $u);
 
-            const WIDTH: usize = Self::BITS as usize;
+        impl ToMif for $f {
+            type Binary = $u;
+            type SignedBinary = $u;
 
-            fn unsigned(&self) -> Self::Unsigned {
+            const WIDTH: usize = <$u>::BITS as usize;
+
+            fn signed(&self) -> Self::SignedBinary {
+                self.to_bits()
+            }
+
+            fn bits(&self) -> Self::Binary {
+                self.to_bits()
+            }
+        }
+    };
+    ($s:ty, $u:ty) => {
+        impl ToMif for $s {
+            type Binary = $u;
+            type SignedBinary = $s;
+
+            const WIDTH: usize = <$u>::BITS as usize;
+
+            fn signed(&self) -> Self::SignedBinary {
+                *self
+            }
+
+            fn bits(&self) -> Self::Binary {
                 self.cast_unsigned()
             }
         }
 
         impl ToMif for $u {
-            type Unsigned = $u;
+            type Binary = $u;
+            type SignedBinary = $u;
 
-            const WIDTH: usize = Self::BITS as usize;
+            const WIDTH: usize = <$u>::BITS as usize;
 
-            fn unsigned(&self) -> Self::Unsigned {
+            fn signed(&self) -> Self::SignedBinary {
                 *self
+            }
+
+            fn bits(&self) -> Self::Binary {
+                *self
+            }
+        }
+
+        impl ToMif for std::num::NonZero<$s> {
+            type Binary = $u;
+            type SignedBinary = $s;
+
+            const WIDTH: usize = <$u>::BITS as usize;
+
+            fn signed(&self) -> Self::SignedBinary {
+                self.get()
+            }
+
+            fn bits(&self) -> Self::Binary {
+                self.get().cast_unsigned()
+            }
+        }
+
+        impl ToMif for std::num::NonZero<$u> {
+            type Binary = $u;
+            type SignedBinary = $u;
+
+            const WIDTH: usize = <$u>::BITS as usize;
+
+            fn signed(&self) -> Self::SignedBinary {
+                self.get()
+            }
+
+            fn bits(&self) -> Self::Binary {
+                self.get()
             }
         }
     };
 }
 
 impl_to_mif!(i8, u8);
+#[cfg(feature = "f16")]
+impl_to_mif!(i16, u16, f16);
+#[cfg(not(feature = "f16"))]
 impl_to_mif!(i16, u16);
-impl_to_mif!(i32, u32);
-impl_to_mif!(i64, u64);
+impl_to_mif!(i32, u32, f32);
+impl_to_mif!(i64, u64, f64);
+#[cfg(feature = "f128")]
+impl_to_mif!(i128, u128, f128);
+#[cfg(not(feature = "f128"))]
 impl_to_mif!(i128, u128);
 impl_to_mif!(isize, usize);
 
